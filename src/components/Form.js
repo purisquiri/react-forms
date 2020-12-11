5c38f52d10a6dfbce6012f67bf84eea10f333eb5
purisquiri <mau.dorigo@outlook.es>
2020-12-11 09:16:47 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,28 +2,57 @@ import React from 'react';
 
 class Form extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Henry"
+    firstName: "",
+    lastName: "",
+    submittedData: []
   }
 
-  handleFirstNameChange = event => {
+  handleChange = event => {
     this.setState({
-      firstName: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
+  // handleLastNameChange = event => {
+  //   this.setState({
+  //     lastName: event.target.value
+  //   })
+  // }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, 
+                      lastName: this.state.lastName
+    }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
     })
+    
   }
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input 
+          type="text"
+          name="firstName" 
+          onChange={event => this.handleChange(event)} value={this.state.firstName} 
+          />
+          <input 
+          type="text"
+          name="lastName"  
+          onChange={event => this.handleChange(event)} value={this.state.lastName} 
+          />
+          <input type="submit" />
+        </form>
+        {this.listOfSubmissions()}
+        </div>
     )
   }
 }
